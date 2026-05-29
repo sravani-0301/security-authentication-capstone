007340455c33bcd4024a784d746d102fd6705087
sravani-0301 <sravanidontham@gmail.com>
2026-05-29 19:20:33 +0530

    security-tests.js

---
+++ security-tests.js
@@ -0,0 +1,17 @@
+console.log(validateInput("admin"));
+
+console.log(
+    validateInput("<script>")
+);
+
+console.log(
+    login("admin", "Secure123")
+);
+
+console.log(
+    checkAccess("admin")
+);
+
+console.log(
+    sanitizeInput("<script>")
+);
